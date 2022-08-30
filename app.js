0e4e8d7d2cd3180abf4966d813edbc6e0cd0f41a
kathrynsayer <kssayer@gmail.com>
2022-08-30 11:29:22 -0700

    first half of APIs

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
+"use strict";
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
+const GIPHY_KEY = "x9aS8zVp9Nnq4LrTGUcuS2QnLa0BsGeU";
+
+let feedbackEle = document.querySelector("#feedback");
+let searchInput = document.querySelector("#searchWord");
+let searchButton = document.querySelector("#submitSearch");
+let gifEle = document.querySelector("#imageContainer > img");
+
+
+searchButton.addEventListener("click", (event) => {
+    getGif(searchInput.value);
+});
+
+function getGif(searchTerm) {
+    fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${searchTerm}`)
+    .then((res) => res.json())
+    .then((body) => {
+    gifEle.src = body.data.images.original.url;
+})
+    .catch((err) => {
+        console.error(err);
+        feedbackEle.textContent = err.message;
+    });
+
+});
+}
+async function getGif(searchTerm) {
+    try {
+        let res = await fetch()
+    }
+}
